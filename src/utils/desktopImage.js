5cd07018c0c98ef3dd3442cc7094ed31a2f93b5a
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-11-09 20:05:14 +0100

    Cretion Added

---
+++ src/utils/desktopImage.js
@@ -0,0 +1,34 @@
+export const imagesdesktop = [
+  {
+    image: require("../assets/desktop/image-deep-earth.jpg"),
+    title: "Deep Earth",
+  },
+  {
+    image: require("../assets/desktop/image-night-arcade.jpg"),
+    title: "Night arcade",
+  },
+  {
+    image: require("../assets/desktop/image-soccer-team.jpg"),
+    title: "Soccer Team VR",
+  },
+  {
+    image: require("../assets/desktop/image-grid.jpg"),
+    title: "The grid",
+  },
+  {
+    image: require("../assets/desktop/image-from-above.jpg"),
+    title: "From up above VR",
+  },
+  {
+    image: require("../assets/desktop/image-pocket-borealis.jpg"),
+    title: "Pocket borealis",
+  },
+  {
+    image: require("../assets/desktop/image-curiosity.jpg"),
+    title: "The curiosity",
+  },
+  {
+    image: require("../assets/desktop/image-fisheye.jpg"),
+    title: "Make it fisheye",
+  },
+];
---
+++ src/utils/mobileImage.js
@@ -0,0 +1,34 @@
+export const imagesMobile = [
+  {
+    image: require("../assets/mobile/image-deep-earth.jpg"),
+    title: "Deep Earth",
+  },
+  {
+    image: require("../assets/mobile/image-night-arcade.jpg"),
+    title: "Night arcade",
+  },
+  {
+    image: require("../assets/mobile/image-soccer-team.jpg"),
+    title: "Soccer Team VR",
+  },
+  {
+    image: require("../assets/mobile/image-grid.jpg"),
+    title: "The grid",
+  },
+  {
+    image: require("../assets/mobile/image-interactive.jpg"),
+    title: "From up above VR",
+  },
+  {
+    image: require("../assets/mobile/image-pocket-borealis.jpg"),
+    title: "Pocket borealis",
+  },
+  {
+    image: require("../assets/mobile/image-curiosity.jpg"),
+    title: "The curiosity",
+  },
+  {
+    image: require("../assets/mobile/image-fisheye.jpg"),
+    title: "Make it fisheye",
+  },
+];
