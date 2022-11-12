877e60dce2350ca06eb551987464100c1db03985
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2022-11-13 00:28:43 +0100

    Mobile Responsiveness Fixed

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import "./Footer.css";
-import Nav from "./../Nav/Nav";
-import Logo from "./../Logo/Logo";
 import facebook from "../../assets/icon-facebook.svg";
 import twitter from "../../assets/icon-twitter.svg";
 import pinterest from "../../assets/icon-pinterest.svg";
 import instagram from "../../assets/icon-instagram.svg";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="container-fluid p-0 m-0 F-Bg">
-      <div className="container">
+      <div className="container p-0">
         <div className="Footer">
           <div className="Footer__H">
-            <Logo />
-            <Nav />
+            <div className="Hero__Logo">loopstudios</div>
+            <div className="Hero__Nav">
+              <ul className="">
+                <li>
+                  <Link>About</Link>
+                </li>
+
+                <li>
+                  <Link>Careers</Link>
+                </li>
+
+                <li>
+                  <Link>Events</Link>
+                </li>
+
+                <li>
+                  <Link>Products</Link>
+                </li>
+
+                <li>
+                  <Link>Support</Link>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className="Footer__B">
             <div className="Footer__B__SocialMedia">
